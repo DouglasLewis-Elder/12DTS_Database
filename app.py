4e683d5038aa6911bc7f618f3a0721a5e03f323a
DouglasLewis-Elder <21086@wc.school.nz>
2024-06-13 11:08:39 +1200

    I added the base.html and style.css for everything to be based off of and database.html and index.html as webpages

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello Worlfududgdogdoyghguidgdgjudjgd!'
 
+def render_index_page():
+    return render_template('index.html')
+
+@app.route('/database')
+
+def render_database_page():
+    return render_template('database.html')
 
 if __name__ == '__main__':
     app.run()
