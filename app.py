c9fd08f1069b9f83320a46c2cd503b78ba9edd4c
21086 <21086@wc.school.nz>
2024-06-17 09:57:47 +1200

    Adding Database, but it wont show the data.

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "webtags.db"
+
+def create_connect(db_filename):
+    try:
+        connection = sqlite3.connect(db_filename)
+        return connection
+    except Error as e:
+        print(e)
+        return None
 
 
 @app.route('/')
-
 def render_index_page():
     return render_template('index.html')
 
-@app.route('/database')
 
+@app.route('/database')
 def render_database_page():
-    return render_template('database.html')
+
+    query = "SELECT generation, engine  FROM web_tags"
+    connection = create_connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute(query, )
+
+    data_list = cursor.fetchall()
+    print(data_list)
+
+    return render_template('database.html', data=data_list)
+
 
 if __name__ == '__main__':
     app.run()
