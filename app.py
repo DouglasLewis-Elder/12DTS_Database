d8f4096ae97436ca2b98783b3c27c880462f37bb
21086 <21086@wc.school.nz>
2024-06-20 11:09:20 +1200

    Almost finished database, but Width is not working for some reason

--- app.py
+++ app.py
@@ -22,10 +22,11 @@ def render_index_page():
 @app.route('/database')
 def render_database_page():
 
-    query = "SELECT generation, engine, start_of_production, body_type, seats, doors, acceleration, maximum_speed, power_to_weight_ratio, weight_to_torque_ratio, power, power_per_litre, torque, engine_layout, engine_displacement FROM porsche_9112"
+    query = "SELECT generation, engine, start_of_production, body_type, seats, doors, acceleration, maximum_speed, power_to_weight_ratio, weight_to_torque_ratio, power, power_per_litre, engine_layout, engine_displacement, number_of_cylinders, engine_configuration, cylinder_bore, piston_stroke, number_of_valves_per_cylinder, engine_aspiration,fuel_tank_capacity, length, width, height, drive_wheel, number_of_gears_and_type_of_gearbox, fuel_consumption_combined, end_of_production  FROM porsche_9112"
     connection = create_connect(DATABASE)
     cursor = connection.cursor()
     cursor.execute(query, )
+
 
     data_list = cursor.fetchall()
     print(data_list)
