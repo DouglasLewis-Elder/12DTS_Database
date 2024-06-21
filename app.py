2ca27e52d6fa8ea7070685dd9482676f66c5d829
DouglasLewis-Elder <21086@wc.school.nz>
2024-06-21 23:38:53 +1200

    Fully Implemented Search feature of the database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 from sqlite3 import Error
 
@@ -22,7 +22,7 @@ def render_index_page():
 @app.route('/database')
 def render_database_page():
 
-    query = "SELECT generation, engine, start_of_production, body_type, seats, doors, acceleration, maximum_speed, power_to_weight_ratio, weight_to_torque_ratio, power, power_per_litre, engine_layout, engine_displacement, number_of_cylinders, engine_configuration, cylinder_bore, piston_stroke, number_of_valves_per_cylinder, engine_aspiration,fuel_tank_capacity, length, width, height, drive_wheel, number_of_gears_and_type_of_gearbox, fuel_consumption_combined, end_of_production  FROM porsche_9112"
+    query = "SELECT generation, engine, start_of_production, body_type, seats, doors, acceleration, maximum_speed, power_to_weight_ratio, weight_to_torque_ratio, power, power_per_litre, engine_layout, engine_displacement, number_of_cylinders, engine_configuration, cylinder_bore, piston_stroke, number_of_valves_per_cylinder, engine_aspiration,fuel_tank_capacity, drive_wheel, number_of_gears_and_type_of_gearbox, fuel_consumption_combined, end_of_production  FROM porsche_9112"
     connection = create_connect(DATABASE)
     cursor = connection.cursor()
     cursor.execute(query, )
@@ -32,6 +32,23 @@ def render_database_page():
     print(data_list)
 
     return render_template('database.html', data=data_list)
+
+@app.route('/search', methods=['GET', 'POST'])
+def render_search_page():
+
+    look_up = request.form['Search']
+    title = "Search for: '" + look_up + "' "
+    look_up = "%" + look_up + "%"
+
+    query = ("SELECT generation, engine, start_of_production, body_type, seats, doors, acceleration, maximum_speed, power_to_weight_ratio, weight_to_torque_ratio, power, power_per_litre, engine_layout, engine_displacement, number_of_cylinders, engine_configuration, cylinder_bore, piston_stroke, number_of_valves_per_cylinder, engine_aspiration,fuel_tank_capacity, drive_wheel, number_of_gears_and_type_of_gearbox, fuel_consumption_combined, end_of_production FROM porsche_9112 WHERE generation LIKE ? OR engine LIKE ? OR start_of_production LIKE ? OR body_type LIKE ? OR seats LIKE ?  OR doors LIKE ? OR acceleration LIKE ? OR maximum_speed LIKE ? OR power_to_weight_ratio LIKE ? OR weight_to_torque_ratio LIKE ? OR power LIKE ? OR power_per_litre LIKE ? OR engine_layout LIKE ? OR engine_displacement LIKE ? OR number_of_cylinders LIKE ? OR engine_configuration LIKE ? OR cylinder_bore LIKE ? OR piston_stroke LIKE ? OR number_of_valves_per_cylinder LIKE ? OR engine_aspiration LIKE ? OR fuel_tank_capacity LIKE ? OR drive_wheel LIKE ? OR number_of_gears_and_type_of_gearbox LIKE ? OR fuel_consumption_combined LIKE ? OR end_of_production LIKE ?")
+    connection = create_connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute(query, (look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up))
+
+    data_list = cursor.fetchall()
+    print(data_list)
+
+    return render_template("search.html", data=data_list)
 
 
 if __name__ == '__main__':
